77eae7f9afd02f4ee135c4bd6573dac2316f34d5
NicolasPP <nicolaspiolaparucker@gmai.com>
2022-04-29 21:26:53 +0200

    instruction_manager clean up

--- instruction.py
+++ instruction.py
@@ -1,7 +1,6 @@
 import os
 from cell import *
 import json
-from time_wait import regular_interval_tick_wait
 
 class Instructions_manager:
     current_instruction = ""
@@ -9,11 +8,14 @@ class Instructions_manager:
         self.index = 0
         self.file = "instructions.json"
         self.read_instructions()
-    
-    def add(self, instruction):
-        if instruction not in self.instructions:
-            self.instructions.append(instruction)
+   
+    # interacting with the json
+    def write_instructions(self):
+        if len(self.instructions) < 1:
+            return
         
+        with open(self.file, "w") as out_file:
+            json.dump(self.instructions, out_file)   
     def read_instructions(self):
         with open(self.file, "r") as open_file:
             if os.stat(self.file).st_size == 0:
@@ -24,16 +26,13 @@ class Instructions_manager:
                 Instructions_manager.current_instruction = self.get_current_instruction()[NAME]
 
             open_file.close()
+    
+    #helper functions
+    def add(self, instruction):
+        if instruction not in self.instructions:
+            self.instructions.append(instruction)
     def get_current_instruction(self):
         return self.instructions[self.index]
-    
-    def write_instructions(self):
-        if len(self.instructions) < 1:
-            return
-        
-        with open(self.file, "w") as out_file:
-            json.dump(self.instructions, out_file)
-
     def handle_optional_resize(self, screen):
         self.current_instruction = self.get_current_instruction()
         optional_size_change  = self.current_instruction[INFO][CELL_S]
@@ -41,27 +40,23 @@ class Instructions_manager:
             handle_set_cell_size(self.current_instruction[INFO][CELL_S], screen)
             generate_cells(screen)
             draw(screen.surface)
-        
-    def perform(self, index, screen):
-        ids = self.current_instruction[INFO][INST]
-        current_func_id = ids[index]
-        func = id_func[current_func_id]
-        func(screen.surface)
-        if index == len(ids) -1:
-            return False
-        return True
-    
     def increase_index(self, surface):
         if self.index + 1 < len(self.instructions):
             self.index += 1
             Instructions_manager.current_instruction = self.get_current_instruction()[NAME]
             draw(surface)
-
-
     def decrease_index(self, surface):
         if self.index > 0:
             self.index -= 1
             Instructions_manager.current_instruction = self.get_current_instruction()[NAME]
             draw(surface)
 
+    #execution
+    def perform(self, index, screen):
+        ids = self.current_instruction[INFO][INST]
+        current_func_id = ids[index]
+        id_func[current_func_id](screen.surface)
+        if index == len(ids) -1:
+            return False
+        return True
             
